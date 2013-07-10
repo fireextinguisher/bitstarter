08c29f1ed405f152c1250fee5411c4acf7e58650
Fireextinguisher <ground@mail.ru>
2013-07-10 21:30:54 +0000

    Added index.html and updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
 
+var fs = require('fs');
+var infile = "index.html";
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+
+  buf = fs.readFileSync(infile);
+  response.send(buf.toString());
+
 });
 
 var port = process.env.PORT || 5000;

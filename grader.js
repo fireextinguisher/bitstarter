e039e3a903880ecb27a1e4f7f5f75cf30766f1b3
Fireextinguisher <ground@mail.ru>
2013-07-17 21:35:46 +0000

    grader.js updated to accept URL.

--- grader.js
+++ grader.js
@@ -21,19 +21,43 @@ References:
    - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
 */
 
+var util = require('util');
 var fs = require('fs');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
+var HTMLFILE_URL = 'http://damp-wave-6510.herokuapp.com';
+var HTMLFILE_TEMP = 'web.html';
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
-        console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+	console.log("%s does not exist. Exiting.", instr);
+	process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
+};
+
+var assertUrlExists = function(inurl, checksfile) {
+    var instr = inurl.toString();
+    rest.get(instr).on('complete', function(result) {
+        if (result instanceof Error) {
+            console.log('Error: ' + util.format(result.message));
+	} else {
+	    var $ = cheerio.load(result);
+	    var checks = loadChecks(checksfile).sort();
+	    var out = {};
+	    for(var ii in checks) {
+		var present = $(checks[ii]).length > 0;
+		out[checks[ii]] = present;
+	    }
+	    outJson = JSON.stringify(out, null, 4);
+	    console.log(outJson);
+	    return outJson;
+	}
+    });
 };
 
 var cheerioHtmlFile = function(htmlfile) {
@@ -49,8 +73,8 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
-        out[checks[ii]] = present;
+	var present = $(checks[ii]).length > 0;
+	out[checks[ii]] = present;
     }
     return out;
 };
@@ -63,12 +87,24 @@ var clone = function(fn) {
 
 if(require.main == module) {
     program
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+	.option('-f, --file [html_file]', 'Path to index.html')
+	.option('-u, --url [html_file_url]', 'URL to index.html')
+	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+	.parse(process.argv);
+
+    if (program.file) {
+	var inputfile = assertFileExists(program.file);
+	var checkJson = checkHtmlFile(inputfile, program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+    }
+    else {
+	if(!program.url) program.url = HTMLFILE_URL;
+	var outJson = assertUrlExists(program.url, program.checks);
+//	var checkJson = checkHtmlFile(inputfile, program.checks);
+    }
+//    var outJson = JSON.stringify(checkJson, null, 4);
+//    console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
